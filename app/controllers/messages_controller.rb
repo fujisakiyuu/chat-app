dd014add0d35fa88ae336d64bbf02b3ef32be9a7
fujisakiyuu <yurayurataro@icloud.com>
2020-12-30 15:40:00 +0900

    チャットルームの削除機能の実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -14,11 +14,12 @@ class MessagesController < ApplicationController
       @messages = @room.messages.includes(:user)
       render :index
   end
+end
 
   private
 
   def message_params
     params.require(:message).permit(:content).merge(user_id: current_user.id)
   end
-end
+
 end
